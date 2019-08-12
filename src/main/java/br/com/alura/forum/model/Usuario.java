a3b7efda1349a13ac1d5f10f9eb9b7c2a15c9995
Danillo Carvalho <dccarvalho@unicid.edu.br>
2019-08-12 16:34:43 -0300

    JPA

--- src/main/java/br/com/alura/forum/model/Usuario.java
+++ src/main/java/br/com/alura/forum/model/Usuario.java
@@ -2,11 +2,12 @@ package br.com.alura.forum.model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 
 @Entity
 public class Usuario {
 
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String email;
