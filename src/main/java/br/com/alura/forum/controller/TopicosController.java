a49503d0ae7314a4d76332a47b35f1c678175de7
Danillo Carvalho <dccarvalho@unicid.edu.br>
2019-08-13 15:36:00 -0300

    Criada interface TopicoRepository

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,22 +1,26 @@
 package br.com.alura.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.alura.forum.controller.dto.TopicoDto;
-import br.com.alura.forum.model.Curso;
+import br.com.alura.forum.dto.TopicoDto;
 import br.com.alura.forum.model.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController
 public class TopicosController {
 	
+	@Autowired
+	private TopicoRepository topicoRepository;
+	
 	@RequestMapping("/topicos")
 	public List<TopicoDto> list(){
-		Topico topico = new Topico("Duvida", "Duvida de Spring", new Curso("Spring", "Programacao"));
-		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+		
+		List<Topico> topicos = topicoRepository.findAll();
+		return TopicoDto.converter(topicos);
 	}
 
 }
